fb2d37823add3ba29fba7bbf9528f34b7d3fdef4
Shion Wah <onishinobimusha@hotmail.com>
2018-06-28 02:37:11 +0800

    #1 Remove SimpleStorage, add ownership to Marketplace contract, get current user

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+/**
+ * @title Marketplace
+ * @dev Marketplace dapp with single owner
+ */
+contract Marketplace is Ownable {
+
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import MarketplaceContract from '../build/contracts/Marketplace.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -12,77 +12,61 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
-      web3: null
+      web3: null,
+      currentUser: {
+        account: null,
+        role: null
+      }
     }
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
-    getWeb3
-    .then(results => {
+    // Get network provider, web3 instance and current active account
+    getWeb3.then(results => {
       this.setState({
-        web3: results.web3
+        web3: results.web3,
       })
 
       // Instantiate contract once web3 provided.
-      this.instantiateContract()
+      this.getCurrentUser()
     })
     .catch(() => {
       console.log('Error finding web3.')
     })
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
+  async getCurrentUser() {
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const marketplace = contract(MarketplaceContract)
+    marketplace.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    const marketplaceInstance = await marketplace.deployed()
+    const ownerAccount = await marketplaceInstance.owner.call()
+    const role = this.state.currentUser.account === ownerAccount ? 'Admin' : 'Shopper'
 
-    // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
-    })
+      this.setState({ 
+        currentUser: {
+          ...this.state.currentUser,
+          account: accounts[0],
+          role: role
+        }
+      })      
+    });
   }
 
   render() {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Decentralized Marketplace</a>
         </nav>
 
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <h1>Hi {this.state.currentUser.account ? this.state.currentUser.account.substr(0,6) : 'User'} ({this.state.currentUser.role})</h1>
+              <p>Ready to shop!</p>
             </div>
           </div>
         </main>
