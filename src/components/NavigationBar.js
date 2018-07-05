f859726ef84fa8142281d63682bee95c58dc1cdf
Shion Wah <onishinobimusha@hotmail.com>
2018-07-06 02:52:36 +0800

    #3 Fix active link highlighting in navigation bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Role from '../constants/role'
 import { getShortAddress } from '../utils/getCurrentUser'
 
 const NavigationBar = ({ user }) => {
   return (
     <nav className="navbar pure-menu pure-menu-horizontal">
-      <Link to="/"><div href="#" className="pure-menu-heading pure-menu-link">Decentralized Marketplace</div></Link>
+      <NavLink exact to="/" className="pure-menu-heading pure-menu-link" activeClassName="pure-menu-link">Decentralized Marketplace</NavLink>
       <ul className="pure-menu-list">
-        {user.role === Role.ADMIN ? (<Link to="/store-owners"><li className="pure-menu-item pure-menu-link">Manage Store Owners</li></Link>) : null}
-        {user.role === Role.STORE_OWNER ? (<Link to="/storefronts"><li className="pure-menu-item pure-menu-link">Manage Storefronts</li></Link>) : null}
-        <Link to="/profile"><li className="pure-menu-item pure-menu-link">{getShortAddress(user.account)} ({user.role})</li></Link>
+        {user.role === Role.ADMIN ? (<NavLink to="/store-owners" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Store Owners</li></NavLink>) : null}
+        {user.role === Role.STORE_OWNER ? (<NavLink to="/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Storefronts</li></NavLink>) : null}
+        <NavLink to="/profile" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>{getShortAddress(user.account)} ({user.role})</li></NavLink>
       </ul>
     </nav>
   )
