6d47caf8520a2651277bfb71700643a5cb4cff66
Shion Wah <onishinobimusha@hotmail.com>
2018-07-06 14:51:12 +0800

    #3 Add table styling to Store Owners listing page

--- src/components/StoreOwners.js
+++ src/components/StoreOwners.js
@@ -1,11 +1,33 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 const StoreOwners = ({ storeOwners }) => {
   return (
     <div className="pure-u-1-1">
       <h1>Store Owner List</h1>
-      {storeOwners.map(storeOwner => <div key={`so-${storeOwner}`} className="pure-u-1-1">{storeOwner}</div>)}
+
+      <table className="pure-table pure-table-horizontal no-border">
+        <thead className="no-background-color">
+            <tr>
+              <th>#</th>
+              <th>Address</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        { 
+          _.map(storeOwners, (storeOwner, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{storeOwner}</td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+      </table>
     </div>
   )
 }
