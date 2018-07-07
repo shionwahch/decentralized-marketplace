fc3a466b423c92e6281054c7b01ebf62787f9715
Shion Wah <onishinobimusha@hotmail.com>
2018-07-08 02:17:29 +0800

    #4 Show shopper functions when user is a shopper

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -10,6 +10,7 @@ const NavigationBar = ({ user }) => {
       <ul className="pure-menu-list">
         {user.role === Role.ADMIN ? (<NavLink to="/store-owners" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Store Owners</li></NavLink>) : null}
         {user.role === Role.STORE_OWNER ? (<NavLink to="/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Storefronts</li></NavLink>) : null}
+        {user.role === Role.SHOPPER ? (<NavLink to="/browse" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Browse Storefronts</li></NavLink>) : null}
         <NavLink to="/profile" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>{getShortAddress(user.account)} ({user.role})</li></NavLink>
       </ul>
     </nav>
