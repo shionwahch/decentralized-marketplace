a4b162390dd32ff4dbc55aba0f0f280273498b1d
shionwah <onishinobimusha@hotmail.com>
2018-07-17 16:20:03 +0800

    #6 Add predefined gas limit for deleting of product

--- src/models/Product.js
+++ src/models/Product.js
@@ -32,7 +32,7 @@ class Product {
   }
   
   static removeProduct =  async (marketplace, id) => {
-    const results = await marketplace.removeProduct(id)
+    const results = await marketplace.removeProduct(id, { gas: 50000 })
     const removedProduct = Product.getProductFromTransaction(results, Event.ProductRemoved)
     return removedProduct
   }
