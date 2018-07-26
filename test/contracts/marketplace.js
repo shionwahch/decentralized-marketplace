eaf2c27e14d3af9ef0cedb706e5aea4943fb4150
shionwah <onishinobimusha@hotmail.com>
2018-07-26 13:47:24 +0800

    #15 Refactor test structure, assertion on vm revert is now working

--- test/contracts/marketplace.js
+++ test/contracts/marketplace.js
@@ -1,4 +1,6 @@
-const Marketplace = artifacts.require("../../contracts/Marketplace.sol")
+// import { assertRevert } from '../helpers/assertRevert'
+const { assertRevert } = require('../helpers/assertRevert');
+const Marketplace = artifacts.require('../../contracts/Marketplace.sol')
 
 contract('Marketplace', (accounts) => {
   
@@ -17,58 +19,72 @@ contract('Marketplace', (accounts) => {
     await marketplace.addStorefront('Store 2', { from: storeOwner1 })
   });
 
-  it("should return owner as the owner of the contract.", async () => {
-    const contractOwner = await marketplace.owner.call()
-    assert.equal(owner, contractOwner, `The contract owner should be ${contractOwner}`);
+  describe('Admin', () => {
+    it('should return owner as the owner (admin) of the contract.', async () => {
+      const contractOwner = await marketplace.owner.call()
+      assert.equal(owner, contractOwner, `The contract owner should be ${contractOwner}`);
+    });
+  })
+
+  describe('Store Owner', () => {
+    describe('storeOwnerCount', () => {
+      it('should return 2 as the store owner count when added 2 store owners.', async () => {
+        const storeOwnerCount = await marketplace.storeOwnerCount.call()
+        assert.equal(storeOwnerCount.toNumber(), 2, `The store owner count should be 2`);
+      });
+    })
+    
+    describe('getStoreOwner', () => {
+      it('should return storeOwner1 when querying with index 0.', async () => {
+        const storeOwner = await marketplace.getStoreOwner.call(0)
+        const storeOwnerAddress = storeOwner[0]
+        assert.equal(storeOwnerAddress, storeOwner1, `The store owner should be ${storeOwner1}`);
+      });
+    
+      it('should return error when retrieving a store owner with negative index number.', async () => {
+        const getStoreOwnerCall = marketplace.getStoreOwner.call(-1)
+        await assertRevert(getStoreOwnerCall);
+      });
+    
+      it('should return error when retrieving a store owner with index out of bounds.', async () => {
+        const getStoreOwnerCall = marketplace.getStoreOwner.call(99)
+        await assertRevert(getStoreOwnerCall);
+      });
+    })
+    
+    describe('getStoreOwnerByAddress', () => {
+      it('should return storeOwner1 when querying with storeOwner1 address.', async () => {
+        const storeOwner = await marketplace.getStoreOwnerByAddress.call(storeOwner1)
+        const storeOwnerAddress = storeOwner[0]
+        assert.equal(storeOwnerAddress, storeOwner1, `The store owner should be ${storeOwner1}`);
+      });
+    })
+
+    describe('addStoreOwner', () => {
+      it('should not allow adding of same store owner.', async () => {
+        const addStoreOwnerCall = marketplace.addStoreOwner(storeOwner1, { from: owner })
+        await assertRevert(addStoreOwnerCall)
+      });
+      
+      it('should return error adding store owner with non-admin account.', async () => {
+        const addStoreOwnerCall = marketplace.addStoreOwner(shopper1, {from: shopper2})
+        await assertRevert(addStoreOwnerCall);
+      });
+    })
   });
 
-  it("should return 2 as the store owner count when added 2 store owners.", async () => {
-    const storeOwnerCount = await marketplace.storeOwnerCount.call()
-    assert.equal(storeOwnerCount.toNumber(), 2, `The store owner count should be 2`);
-  });
-
-  // TODO Unable to import and use open-zeppelin packages
-  it.skip("should not allow adding of same store owner.", async () => {
-    await marketplace.addStoreOwner(storeOwner1, { from: owner })
-    await assertRevert(await marketplace.addStoreOwner(storeOwner1, { from: owner }))
-  });
+  describe('Storefront', () => {
+    it('should return Store 1 details.', async () => {
+      const storefrontResults = await marketplace.getStorefront.call(0)
+      const name = storefrontResults[1]
+      assert.equal(name, 'Store 1', `The store owner count should be Store 1`);
+    });
   
-  // TODO Unable to import and use open-zeppelin packages
-  it.skip("should return error adding store owner with non-admin account.", async () => {
-    const addStoreOwner = marketplace.addStoreOwner(shopper1, {from: shopper2})
-    await assertRevert(await addStoreOwner);
+    it('should return [0, 1] (index of storefront).', async () => {
+      const storeOwner = await marketplace.getStoreOwner.call(0)
+      const storefronts = storeOwner[1].map(s => s.toNumber())
+      assert.equal(storefronts[0], 0, `The storefront index should be 0`);
+      assert.equal(storefronts[1], 1, `The storefront index should be 1`);
+    });
   });
-
-  it("should return storeOwner1.", async () => {
-    const storeOwner = await marketplace.getStoreOwner.call(0)
-    const owner = storeOwner[0]
-    assert.equal(owner, storeOwner1, `The store owner should be ${storeOwner1}`);
-  });
-
-  // TODO Unable to import and use open-zeppelin packages
-  it.skip("should return error when retrieving a store owner with index -1.", async () => {
-    const storeOwner = marketplace.getStoreOwner.call(-1)
-    await assertRevert(await storeOwner);
-  });
-
-  // TODO Unable to import and use open-zeppelin packages
-  it.skip("should return error when retrieving a store owner with index out of bounds.", async () => {
-    const storeOwnerCount = await marketplace.storeOwnerCount.call()
-    const storeOwner = await marketplace.getStoreOwner.call(storeOwnerCount)
-    await assertRevert(await storeOwner);
-  });
-
-  it("should return Store 1 details.", async () => {
-    const storefront = await marketplace.getStorefront.call(0)
-    const name = storefront
-    assert.equal(name, 'Store 1', `The store owner count should be Store 1`);
-  });
-
-  it("should return [0, 1] (index of storefront).", async () => {
-    const storeOwner = await marketplace.getStoreOwner.call(0)
-    const storefronts = storeOwner[1].map(s => s.toNumber())
-    assert.equal(storefronts[0], 0, `The storefront index should be 0`);
-    assert.equal(storefronts[1], 1, `The storefront index should be 1`);
-  });
-
 });
