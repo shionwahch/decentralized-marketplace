d3d69aeda392f714816505612249619e5b85b9ee
Shion Wah <onishinobimusha@hotmail.com>
2018-07-29 19:02:43 +0800

    #24 Add hash-generated icon for user

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
+import blockies from '../js/blockies.min'
 import { isAdmin, isStoreOwner, isShopper } from '../constants/role'
 import { getShortAddress } from '../utils/getCurrentUser'
 
 const NavigationBar = ({ user }) => {
+  const icon = blockies.create({
+    seed: user.account,
+    bgcolor: '#fff',
+  })
+
   return (
     <nav className="navbar pure-menu pure-menu-horizontal">
       <NavLink exact to="/" className="pure-menu-heading pure-menu-link" activeClassName="pure-menu-link">Decentralized Marketplace</NavLink>
       <ul className="pure-menu-list">
-        {isAdmin(user.role) ? (<NavLink to="/manage/store-owners" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Store Owners</li></NavLink>) : null}
-        {isStoreOwner(user.role) ? (<NavLink to="/manage/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Storefronts</li></NavLink>) : null}
-        {isShopper(user.role) ? (<NavLink to="/browse/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Browse Storefronts</li></NavLink>) : null}
+        { isAdmin(user.role) ? (<NavLink to="/manage/store-owners" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Store Owners</li></NavLink>) : null }
+        { isStoreOwner(user.role) ? (<NavLink to="/manage/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Manage Storefronts</li></NavLink>) : null }
+        { isShopper(user.role) ? (<NavLink to="/browse/storefronts" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>Browse Storefronts</li></NavLink>) : null }
+      </ul>
+      <ul className="pure-menu-list navbar-right">
+        <img className="profile-icon" src={icon.toDataURL()}/>
         <NavLink to="/profile" className="pure-menu-item pure-menu-link" activeClassName="pure-menu-link"><li>{getShortAddress(user.account)} ({user.role})</li></NavLink>
       </ul>
     </nav>
