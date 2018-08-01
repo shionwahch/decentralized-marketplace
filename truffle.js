44c81c4ebb58b762c39d6f001cb1b0bb90db2c26
shionwah <onishinobimusha@hotmail.com>
2018-08-01 14:55:25 +0800

    #N/A Added Rinkeby network configuration

--- truffle.js
+++ truffle.js
@@ -4,6 +4,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: `https://rinkeby.infura.io/${process.env.REACT_APP_INFURA_API_KEY}`,
+      network_id: 4
     }
   }
 };
